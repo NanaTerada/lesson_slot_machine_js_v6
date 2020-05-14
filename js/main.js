14eabc89665d2ebb4836fb0a2b17eff8b0af1ef7
n2 <7worki.nprogress@gmail.com>
2020-05-14 15:03:44 +0900

    #11 結果判定処理を完成させよう

--- js/main.js
+++ js/main.js
@@ -20,7 +20,8 @@
                 clearTimeout(this.timeoutId);  //???
 
                 panelsLeft--;
-                if(panelsLeft === 0) {
+
+                if (panelsLeft === 0) {
                     checkResult();
                 }
             });
@@ -40,7 +41,7 @@
             'img/bell.png',
             'img/cherry.png'
         ];
-        return images[Math.floor(Math.random() * images.length)]
+        return images[Math.floor(Math.random() * images.length)];
     }
 
     spin() {
@@ -50,23 +51,39 @@
         },50);
     }
 
+
+
+    isUnmatched(p1,p2){
+        // if (this.img.src !== p1.img.src && this.img.src !== p2.img.src) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        return this.img.src !== p1.img.src && this.img.src !== p2.img.src;
+    }
+
+    unmatch() {
+        this.img.classList.add('unmatched');
+    }
+
+}
+
     // パネル同士を比較するのでPanel クラスの外にかく
-    function checkResult() {
-      if(panels[0].isUnmatched(panels[1],panels[2])) {
+      function checkResult() {
+      if(panels[0].isUnmatched(panels[1],panels[2])) {  
         panels[0].unmatch();
       } 
-      if(panels[1].isUnmatched(panels[0],panels[2])) {
-        panels[0].unmatch();
+      else if(panels[1].isUnmatched(panels[0],panels[2])) { //if3回使っていいのか？
+        panels[1].unmatch();
       }  
-      if(panels[2].isUnmatched(panels[0],panels[1])) {
-        panels[0].unmatch();
+      else if(panels[2].isUnmatched(panels[0],panels[1])) {
+        panels[2].unmatch();
       }  
       
     };
 
 
 
-    }
         // html消してインスタンスをつくる
     const panels = [
         new Panel(),
